28ce7cbda5a970d2eadb1ac56bcf458a2ef59cf9
paplauskis <paplauskaskostas@gmail.com>
2023-05-31 17:44:30 +0300

    added menu tab structure

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,87 @@
-export const loadMenu = (() => {
-  
-})();
+export const loadMenu = (load) => {
+  load.innerHTML = `<div class="menu">
+  <h2>Wok</h2>
+  <div class="woks dishes">
+    <div class="wok1 dish-item">
+      <h4>Ebi Curry</h4>
+      <img src="../assets/wok-ebi-curry.jpeg" alt="ebi curry">
+      <p>Made by my indian friend</p>
+      <p>10.99$</p>
+    </div>
+    <div class="wok2 dish-item">
+      <h4>Gju Soba</h4>
+      <img src="../assets/wok-gju-soba.jpeg" alt="gju soba">
+      <p>Spicy, be careful</p>
+      <p>9.99$</p>
+    </div>
+    <div class="wok3 dish-item">
+      <h4>Pork Teriyaki Jaki Soba</h4>
+      <img src="../assets/wok-pork-teriyaki-jaki-soba.jpeg" alt="pork teriyaki jaki soba">
+      <p>Some people have strong diarrhea after this</p>
+      <p>9.99$</p>
+    </div>
+    <div class="wok4 dish-item">
+      <h4>Savory Eggplants</h4>
+      <img src="../assets/wok-savory-eggplants.jpeg" alt="savory eggplants">
+      <p>Tasty for some people</p>
+      <p>8.99$</p>
+    </div>
+  </div>
+  <hr>
+  <h2>Donburi</h2>
+  <div class="donburi dishes">
+    <div class="donburi1 dish-item">
+      <h4>Gju Teriyaki</h4>
+      <img src="../assets/gju-teriyaki-donburi.jpeg" alt="gju teriyaki">
+      <p>Our personal favourite</p>
+      <p>9.99$</p>
+    </div>
+    <div class="donburi2 dish-item">
+      <h4>Tori Teriyaki</h4>
+      <img src="../assets/tori-teriyaki-donburi.jpeg" alt="tori teriyaki">
+      <p>After eating this you will want a cocktail</p>
+      <p>9.99$</p>
+    </div>
+    <div class="donburi3 dish-item">
+      <h4>Teriyaki Ahibu</h4>
+      <img src="../assets/teriyaki-ahiru-donburi.jpeg" alt="teriyaki ahiru">
+      <p>Either a hit or miss</p>
+      <p>10.99$</p>
+    </div>
+    <div class="donburi4 dish-item">
+      <h4>Kaisen Donburi</h4>
+      <img src="../assets/kaisen-donburi.jpeg" alt="kaisen donburi">
+      <p>People favourite</p>
+      <p>10.99$</p>
+    </div>
+  </div>
+  <hr>
+  <h2>Elixirs</h2>
+  <div class="drinks dishes">
+    <div class="drink1 dish-item">
+      <h4>Cosmopolitan</h4>
+      <img src="../assets/drink-cosmopolitan.webp" alt="Cosmopolitan">
+      <p>You'll start seeing hallucinations</p>
+      <p>7.99$</p>
+    </div>
+    <div class="drink2 dish-item">
+      <h4>Dirty Shirley</h4>
+      <img src="../assets/drink-dirty-shirley.webp" alt="Dirty Shirley">
+      <p>After eating this you will want to get a girl</p>
+      <p>7.99$</p>
+    </div>
+    <div class="drink3 dish-item">
+      <h4>Farmer</h4>
+      <img src="../assets/drink-farmer.webp" alt="Farmer">
+      <p>Unlocks ability to talk to cows</p>
+      <p>7.99$</p>
+    </div>
+    <div class="drink4 dish-item">
+      <h4>Mule</h4>
+      <img src="../assets/drink-mule.webp" alt="Mule">
+      <p>Just sparkling water, lime and vodka</p>
+      <p>6.99$</p>
+    </div>
+  </div>
+</div>`
+};
