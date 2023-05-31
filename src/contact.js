4d58a418470cf487230c0e2ca2e961f73b944254
paplauskis <paplauskaskostas@gmail.com>
2023-05-31 17:41:17 +0300

    added structure for the contact tab

---
+++ src/contact.js
@@ -0,0 +1,11 @@
+export const loadContact = (load) => {
+  load.innerHTML = `<div class="contact">
+  <h4>Have questions?</h4>
+  <h4>Want to book your spot?</h4>
+  <h4>Contact us</h4>
+  <br>
+  <p>+37064616969</p>
+  <p>info@wickedwok.com</p>
+  <p>46, Salako Street, Oluyole Estate, Ibadan</p>
+</div`
+}
